8de0f7cbae7f9f1cee1bc7be6983ae1387799157
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2023-05-19 18:58:25 +0200

    feat: unify scaffold command with prompt
    As discussed in a separate channel, this unifies the scaffold command into a
    single prompt.  This prevents us from polluting the CLI commands with too many
    different commands, and gives us the option to choose better names later.
    
    It seems that this command will be renamed to init but that's out of scope
    for this quick PR.

--- internal/scaffold/rule.go
+++ internal/scaffold/rule.go
@@ -15,16 +15,11 @@
 package scaffold
 
 import (
-	"fmt"
-
 	"github.com/snyk/cli-extension-iac-rules/internal/project"
 	"github.com/snyk/cli-extension-iac-rules/internal/scaffold/forms"
 	"github.com/snyk/go-application-framework/pkg/workflow"
 	"github.com/spf13/afero"
-	"github.com/spf13/pflag"
 )
-
-var RuleWorkflowID = workflow.NewWorkflowIdentifier("iac.scaffold.rule")
 
 func RuleWorkflow(
 	ictx workflow.InvocationContext,
@@ -47,13 +42,4 @@ func RuleWorkflow(
 		return nil, err
 	}
 	return []workflow.Data{}, nil
-}
-
-func RegisterRuleWorkflow(e workflow.Engine) error {
-	flagset := pflag.NewFlagSet("snyk-cli-extension-iac-scaffold-rule", pflag.ExitOnError)
-	c := workflow.ConfigurationOptionsFromFlagset(flagset)
-	if _, err := e.Register(RuleWorkflowID, c, RuleWorkflow); err != nil {
-		return fmt.Errorf("error while registering 'iac scaffold rule' workflow: %w", err)
-	}
-	return nil
 }
